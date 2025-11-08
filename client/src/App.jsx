a82ef90ed34a289621acb328cc20cbde0eec532c
Reen <maureenmukami531@gmail.com>
2025-11-08 18:00:36 +0300

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hello from './components/Hello';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Testable MERN App</h1>
+      <Hello name="Tester" />
+    </div>
+  );
+}
